8d191db5f644175c6268b88599ecd7c7a16943d9
RobCrawford527 <rcrawford527@hotmail.co.uk>
2020-08-13 10:54:33 +0100

    Example function for Programming Assignment 2

---
+++ example.R
@@ -0,0 +1,37 @@
+makeVector <- function(x = numeric()) {
+	m <- NULL
+	set <- function(y) {
+		x <<-y
+		m <<- NULL
+	}
+	get <- function() x
+	setmean <- function(mean) m <<- mean
+	getmean <- function() m
+	list(set = set, get = get, setmean = setmean, getmean = getmean)
+}
+
+
+z <- makeVector(1:5)
+
+z$get()
+z$getmean()
+
+
+cachemean <- function(x, ...) {
+	m <- x$getmean()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- mean(data, ...)
+	x$setmean(m)
+	m
+}
+
+cachemean(makeVector(1:5))
+cachemean(z)
+
+
+
+
